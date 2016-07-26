7ead9eb29578ef768df8b5dc68e8fb3cb21b6bd3
Sean <fastbeanau@gmail.com>
2016-07-27 03:41:59 +1000

    Comment server paths.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,37 @@
 'use strict';
 
-const config = require('config').get('server');
 const skeleton = require('swagger-service-skeleton');
 
 const instanceGenerator = () => skeleton({
   ioc: {
-    autoRegister: { pattern: './services/*.js', 
+    /*
+       This path is relative to *this* file, which is in the ./src directory, thus it is:
+       ./src/services/*.js
+    */
+    autoRegister: { pattern: './services/*.js',
                     rootDirectory: __dirname },
   },
   codegen: {
+    /*
+       This is a generated directory - this is where all of the glue created by the skeleton lives.
+       It is relative to the application (repository) root directory.
+    */
     temporaryDirectory: './dist/codegen',
+    /*
+       This path is relative from the directory defined by temporaryDirectory, which resolves to:
+       ./src/controllers
+    */
     templateSettings: {
       implementationPath: '../../../src/controllers',
     },
   },
   service: {
+    /*
+       This is the path to your YAML file. It is relative to the application (repository) root 
+       directory.
+    */
     swagger: './src/contracts/swagger.yaml',
-    listenPort: config.listenPort,
+    listenPort: 10010,
   },
 });
 
